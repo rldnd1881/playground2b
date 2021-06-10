afc5210c11a3c3b7121d13944e0278b6d4b59059
rldnd111 <rldnd111@naver.com>
2021-06-10 17:43:33 +0900

    팀장의 첫번째 commit

---
+++ src/main/java/team/spring/project/playground2b/IndexController.java
@@ -0,0 +1,13 @@
+package team.spring.project.playground2b;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
